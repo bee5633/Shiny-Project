63d8c3a7867939f26d88078e2d39786ea8e0959b
bee5633 <be5633@gmail.com>
2019-10-11 12:25:14 -0400

    chart 3 and 4 fixed and working

--- Border/server.R
+++ Border/server.R
@@ -1,26 +1,32 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+shinyServer(function(input, output,session) {
+  
+  #Body3 : Show the Data ####
+  output$table <- DT::renderDataTable({
+    datatable(borderF,rownames = FALSE) %>% 
+      formatStyle(input$selected,background="skyblue",fontWeight='bold')
+  })
+  
+  #Body4 : Map ####
+  
+  
+  #Body7 chart 3: By Year ####
+  output$plot3 <- renderPlot(
+    borderF %>%
+      group_by(Border, Year) %>% 
+      summarise(total_3 = sum(Value)) %>% 
+      filter(between(Year, input$Year_3[1], input$Year_3[2])) %>% 
+      ggplot(aes(x=Year, y=total_3)) +
+      geom_col(aes(fill=Border), position="dodge") 
+  ) 
+  
+  #Body8 chart 4: By Month ####
+  output$plot4 <- renderPlot(
+    borderF %>%
+      group_by(Border, Month) %>% 
+      summarise(total_4 = sum(Value)) %>% 
+      filter(between(Month, input$Month_4[1], input$Month_4[2])) %>% 
+      ggplot(aes(x=Month, y=total_4)) +
+      geom_col(aes(fill=Border), position="dodge") 
+  )
 })
+
